256b43411c7508154b98bcf8ee765539fa526a2c
shubham860 <shubham.chauhan860@gmail.com>
2019-09-14 23:32:11 +0530

    2

--- Patterns.c
+++ Patterns.c
@@ -4,7 +4,15 @@ Welcome to GDB Online.
 GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
 C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
 Code, Compile, Run and Debug online from anywhere in world.
-
+*
+**
+***
+****
+*****
+****
+***
+**
+*
 *******************************************************************************/
 #include <stdio.h>
 
@@ -25,6 +33,54 @@ int main()
         }
         else k--;
 
+        printf("\n");
+    }
+
+    return 0;
+}
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+   *
+  **
+ ***
+****
+ ***
+  **
+   *
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j,k=1,s;
+
+    scanf("%d",&n);
+    s=n-1;
+    for(i=1;i<=n*2;i++)
+    {
+        for(j=1;j<=s;j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=k;j++)
+        {
+            printf("*");
+        }
+        if(i<n)
+        {
+            k++;
+            s--;
+        }
+        else{
+            k--;
+            s++;
+        }
         printf("\n");
     }
 
